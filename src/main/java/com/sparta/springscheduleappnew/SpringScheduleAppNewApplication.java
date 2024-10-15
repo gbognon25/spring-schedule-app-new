548789291bde1e7bf678644b4875aff4765a2066
soung90 <sounglikane@gmail.com>
2024-10-15 14:32:57 +0900

    Init commit

---
+++ src/main/java/com/sparta/springscheduleappnew/SpringScheduleAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springscheduleappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleAppNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleAppNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springscheduleappnew/SpringScheduleAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springscheduleappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScheduleAppNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
